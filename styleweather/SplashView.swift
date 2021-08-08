845f8ceeaed9aa6bad741fdfd45b8d61a72aa68b
minju kang <57933061+minjuu@users.noreply.github.com>
2021-08-09 03:41:13 +0900

    날씨 api + scroll view

--- styleweather/SplashView.swift
+++ styleweather/SplashView.swift
@@ -6,30 +6,80 @@
 //
 
 import SwiftUI
+import SwiftUI
+import Combine
+import CoreLocation
 
+
+var curtemp = 0.0
+var icon = ""
+var des = ""
+
+struct WeatherData : Decodable {
+    let coord : Coordinate
+    let cod, visibility, id : Int
+    let name : String
+    let base : String
+    let weather : [Weather]
+    let sys : Sys
+    let main : Main
+    let wind : Wind
+    let dt : Date
+}
+
+struct Coordinate : Decodable {
+    let lat, lon : Double
+}
+
+struct Weather : Decodable {
+    let id : Int
+    let icon : String
+    let main : MainEnum
+    let description: String
+}
+
+struct Sys : Decodable {
+    let type, id : Int
+    let sunrise, sunset : Date
+    let country : String
+}
+
+struct Main : Decodable {
+    let temp, tempMin, tempMax : Double
+    let pressure, humidity : Int
+}
+
+struct Wind : Decodable {
+    let speed : Double
+    let deg : Double?
+    let gust : Double?
+}
+
+enum MainEnum: String, Decodable {
+    case clear = "Clear"
+    case clouds = "Clouds"
+    case rain = "Rain"
+}
 struct SplashView: View {
     
-    // 1.
     @State var isActive:Bool = false
     
     var body: some View {
         VStack {
-            // 2.
+            Text("")
+                .onAppear(){
+                    loadData()
+                }
             if self.isActive {
-                // 3.
                 mainUI()
             } else {
-                // 4.
                 Image("logo")
                     .resizable()
                     .frame(width: 180, height: 142)
             }
         }
-        // 5.
         .onAppear {
-            // 6.
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                // 7.
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 withAnimation {
                     self.isActive = true
                 }
@@ -39,6 +89,44 @@ struct SplashView: View {
     
 }
 
+
+func loadData() {
+    guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=fa6cc892ae147813637c852e0d803bd5") else {
+        fatalError("Invalid URL")
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            decoder.dateDecodingStrategy = .secondsSince1970
+            let weatherData = try decoder.decode(WeatherData.self, from: data)
+            print("Coordinates: ", weatherData.coord)
+            print("Longitude: ", weatherData.coord.lon)
+            print("Latitude: ", weatherData.coord.lat)
+            print("Weather: ", weatherData.weather)
+            print("Temperature: ", weatherData.main.temp - 273.15)
+            print("Temperature_Max: ", weatherData.main.tempMax - 273.15)
+            print("Temperature_Min: ", weatherData.main.tempMin - 273.15)
+            weatherData.weather.forEach {
+                print("icon: ", $0.icon)
+                icon = $0.icon
+                print("description: ", $0.description)
+                des = $0.description
+            }
+            curtemp = weatherData.main.temp - 273.15
+
+        } catch {
+            print("Error serializing Json: ", error)
+        }
+        
+        
+    }.resume()
+}
 
 
 
--- styleweather/mainUI.swift
+++ styleweather/mainUI.swift
@@ -7,24 +7,116 @@
 
 import Foundation
 import SwiftUI
+import Combine
+import CoreLocation
+
 
 struct mainUI: View {
+    var strtemp = String(Int(curtemp))
+    var userData = posts
     var body: some View {
-//        Image(systemName: "flame.fill")
-//            .font(.system(size: 200))
-//            .foregroundColor(.pink)
-//            .shadow(color:.gray,radius: 2, x:2,y:10)
         VStack
         {
-            Image("logo2")
+            HStack(alignment: .center, spacing: 25)
+            {
+                Text("   "+strtemp+"°C")
+                    .font(.custom("Montserrat-ExtraLight", size: 50))
+                    .frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Image(icon.trimmingCharacters(in: .whitespaces))
                 .resizable()
-                .frame(width: 100, height: 80)
-            Text("main page 입니다!")
+                .frame(width: 200, height: 200, alignment: .trailing)
+            }
+            
+            }
+            VStack(alignment:.leading, spacing:20 )
+            {
+                Text("#추천 OOTD 상의                                            ")
+                    .font(.custom("Cafe24SsurroundAir", size: 20))
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                //userData 를 순환하면서 PostView 들을 HStack 에 넣고 ScrollView 생성
+                                ForEach(userData) { post in
+                                    PostView(post: post)
+                                }
+                            }
+                        }
+                Text("#추천 OOTD 하의                                               ")
+                    .font(.custom("Cafe24SsurroundAir", size: 20))
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                //userData 를 순환하면서 PostView 들을 HStack 에 넣고 ScrollView 생성
+                                ForEach(userData) { post in
+                                    PostView(post: post)
+                                }
+                            }
+                        }
+            
+            }
+            
         }
     }
+    
+    
+
 struct mainUI_Previews: PreviewProvider {
     static var previews: some View {
         mainUI()
     }
 }
+
+struct Post : Identifiable {
+    let id = UUID()
+    let title : String
+    let genre : String
+    let runningTime: String
+    let producer : String
+    let casting : String
+    let posterName: String
+}
+//테스트용 영화 정보
+let posts = [
+    Post(title: "다만 악에서 구하소서", genre: "범죄 액션", runningTime: "108분" , producer: "홍원찬", casting: "황정민, 이정재", posterName: "movie_image"),
+    Post(title: "오케이 마담", genre: "코미디, 액션", runningTime: "108분" , producer: "이철하", casting: "엄정화, 박성웅", posterName: "movie_image2"),
+    Post(title: "테넷", genre: "액션 SF", runningTime: "150분" , producer: "크리스토퍼 놀란", casting: "존 데이비드 워싱턴, 로버트 패틴슨", posterName: "movie_image3"),
+]
+
+
+
+struct PostView: View {
+    let post : Post
+    var body: some View {
+        VStack {
+            HStack{
+                Image(post.posterName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.system(size: 16, weight: .bold))
+                    HStack {
+                        Text("개요: \(post.genre)")
+                            .font(.system(size: 12, weight: .light))
+                        Text(post.runningTime)
+                            .font(.system(size: 12, weight: .light, design: .default))
+                    }
+                    Text("감독: \(post.producer)")
+                        .font(.system(size: 12, weight: .light))
+                    Text("출연: \(post.casting)")
+                        .font(.system(size: 12, weight: .light))
+                    Spacer()
+                }.padding(.top, 10)
+                //.padding()
+            }
+        }
+        .padding()
+        .frame(height: 150)
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3742149392, green: 0.3621929838, blue: 0.919604783, alpha: 0.8244663292)), Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        )
+        .foregroundColor(.white)
+        .cornerRadius(15.0)
+    }
 }
